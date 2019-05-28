7c0986150335586acf80612b148d16d63569a82b
Jason Schwartzman <ymschwartzman@gmail.com>
2019-05-28 11:37:43 +0300

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,23 @@
+class Car(object):
+    wheels = 4
+    fuel = "diesel"
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def print(self):
+        print("Your car is: ", self.make, self.model, self.fuel, self.wheels, "wheels")
+
+mustang = Car('Ford', 'Mustang')
+print(type(mustang))
+print(mustang.wheels)
+print(mustang.make)
+mustang.print()
+mercedes = Car('Benz', 'Class A')
+print(type(mercedes))
+print(mercedes.wheels)
+mercedes.wheels = 8
+print(mercedes.window)
+print(mercedes.make)
+mercedes.print()
