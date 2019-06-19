2cb5ddbfa2e2a0ed9eba90959eda59dbc286f48c
yaakovschwartzman <ymschwartzman@gmail.com>
2019-06-19 11:56:39 +0300

    final sqlalchemy example

--- mvc example including json and sqlite/controller_example.py
+++ mvc example including json and sqlite/controller_example.py
@@ -5,7 +5,8 @@ def showAll():
    #gets list of all Person objects
    #people_in_db = Person.getAll()
    #people_in_db = Person.getAllDB()
-   people_in_db = Person.getAllFirestore()
+   people_in_db = Person.getAllDBSQLALCHEMY()
+   #people_in_db = Person.getAllFirestore()
    #calls view
    return view.showAllView(people_in_db)
 
--- mvc example including json and sqlite/model_example.py
+++ mvc example including json and sqlite/model_example.py
@@ -1,22 +1,22 @@
 import json
 import sqlite3
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-import mysql.connector
+import sqlalchemy as db
+#import firebase_admin
+#from firebase_admin import credentials
+#from firebase_admin import firestore
 
-def open_firebase():
-    #cred = credentials.Certificate("/Users/yaakovschwartzman/PycharmProjects/new_developers-institute/mvc example including json and sqlite/mvc-example-85de8-firebase-adminsdk-j8015-99aa57407c.json")
-    cred = credentials.Certificate("/Users/yaakovschwartzman/PycharmProjects/new_developers-institute/mvc example including json and sqlite/my-new-cloud-database-firebase-adminsdk-mdx7u-c463d4be88.json")
-    #firebase_admin.initialize_app(cred, {
-    #    'projectId': 'mvc-example-85de8',
-    #})
-    firebase_admin.initialize_app(cred, {
-       'projectId': 'my-new-cloud-database',
-    })
+#def open_firebase():
+#    #cred = credentials.Certificate("/Users/yaakovschwartzman/PycharmProjects/new_developers-institute/mvc example including json and sqlite/mvc-example-85de8-firebase-adminsdk-j8015-99aa57407c.json")
+#    cred = credentials.Certificate("/Users/yaakovschwartzman/PycharmProjects/new_developers-institute/mvc example including json and sqlite/my-new-cloud-database-firebase-adminsdk-mdx7u-c463d4be88.json")
+#    #firebase_admin.initialize_app(cred, {
+#    #    'projectId': 'mvc-example-85de8',
+#    #})
+#    firebase_admin.initialize_app(cred, {
+#       'projectId': 'my-new-cloud-database',
+#    })
 
-    db = firestore.client()
-    return db
+#    db = firestore.client()
+#    return db
 
 
 def open_rds_connection():
@@ -60,6 +60,8 @@ class Person(object):
             result.append(person)
         return result
 
+
+
     @classmethod
     def getAllDB(self):
         conn = open_database_connection()
@@ -74,20 +76,39 @@ class Person(object):
         return result
 
     @classmethod
-    def getAllFirestore(self):
-        db = open_firebase()
-        # Then query for documents
-        person_ref = db.collection(u'person')
-        docs = person_ref.get()
+    def getAllDBSQLALCHEMY(self):
+        engine = db.create_engine('sqlite:///people.db')
+        # Create a bridge (or connection) with the database through the engine object
+        connection = engine.connect()
+        metadata = db.MetaData()
+        persons = db.Table('person', metadata, autoload=True, autoload_with=engine)
+        query = db.select([persons])
+        record_list = connection.execute(query).fetchall()
         result = []
-        for doc in docs:
-            item = doc.to_dict()
-            person = Person(item['last_name'], item['first_name'], item['phone'], doc.id)
+        #  PSST:   You really don't need to use the object person anymore... you have record_list with everything....
+        # Take a look
+        for item in record_list:
+            person = Person(item[0], item[1], item[2], item[3])
             result.append(person)
         return result
 
-    def addNewFirestore(self, last_name, first_name):
-        db = open_firebase()
-        person_ref = db.collection(u'person')
-        doc_ref = person_ref.document()
-        doc_ref.set({u'first_name': first_name, u'last_name': last_name})
+
+
+#    @classmethod
+#    def getAllFirestore(self):
+#        db = open_firebase()
+#        # Then query for documents
+#        person_ref = db.collection(u'person')
+#        docs = person_ref.get()
+#        result = []
+#        for doc in docs:
+#            item = doc.to_dict()
+#            person = Person(item['last_name'], item['first_name'], item['phone'], doc.id)
+#            result.append(person)
+#        return result
+
+#    def addNewFirestore(self, last_name, first_name):
+#        db = open_firebase()
+#        person_ref = db.collection(u'person')
+#        doc_ref = person_ref.document()
+#        doc_ref.set({u'first_name': first_name, u'last_name': last_name})
